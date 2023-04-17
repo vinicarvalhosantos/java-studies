2c7bde7672bb476f535575cc13748067d3147f30
Vinicius Carvalho dos Santos <vinicius.santos@ibratan.com.br>
2023-04-17 17:26:58 -0300

    Adding an example of how measure the throughput latency in a http server

---
+++ src/main/java/br/com/santos/vinicius/multithreading/examples/ThroughputHttpServerExample.java
@@ -0,0 +1,82 @@
+package br.com.santos.vinicius.multithreading.examples;
+
+import br.com.santos.vinicius.util.FileUtil;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class ThroughputHttpServerExample {
+
+    public static final String INPUT_FILE = FileUtil.getMainFolderWithPath("resources/war_and_peace.txt");
+    public static final int NUMBER_OF_THREADS = 4;
+
+    public static void main(String[] args) throws IOException {
+
+        String text = new String(Files.readAllBytes(Paths.get(INPUT_FILE)));
+        startServer(text);
+    }
+
+    public static void startServer(String text) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/search", new WordCountHandler(text));
+
+        Executor executor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+        server.setExecutor(executor);
+        server.start();
+    }
+
+    private static class WordCountHandler implements HttpHandler {
+
+        private String text;
+
+        public WordCountHandler(String text) {
+            this.text = text;
+        }
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String query = exchange.getRequestURI().getQuery();
+            String[] keyValue = query.split("=");
+            String action = keyValue[0];
+            String word = keyValue[1];
+
+            if (!action.equals("word")) {
+                exchange.sendResponseHeaders(400, 0);
+                return;
+            }
+
+            long count = countWord(word);
+
+            byte[] response = Long.toString(count).getBytes();
+            exchange.sendResponseHeaders(200, response.length);
+            OutputStream outputStream = exchange.getResponseBody();
+            outputStream.write(response);
+            outputStream.close();
+        }
+
+        private long countWord(String word) {
+            long count = 0;
+            int index = 0;
+            while (index >= 0) {
+                index = text.indexOf(word, index);
+
+                if (index >= 0) {
+                    count++;
+                    index++;
+                }
+            }
+
+            return count;
+        }
+    }
+
+
+}
